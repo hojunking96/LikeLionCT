ad0582e64fbc5c693846fe117e3ce25953016cd5
hojunking96 <hojunking96@gmail.com>
2023-02-23 10:03:45 +0900

    문제 풀이1

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,7 +1,20 @@
 package com.ll;
 
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("바꿔보자2");
+        System.out.println(new Solution().solution(10));
+    }
+}
+
+
+class Solution {
+    public int solution(int n) {
+        int sum = 0;
+        for (int i = 0; i <= n; i += 2) {
+            sum += i;
+        }
+        return sum;
     }
 }
