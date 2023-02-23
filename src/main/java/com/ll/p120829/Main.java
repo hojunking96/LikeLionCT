9431cb4c103648912812f17c50780260f54bed46
hojunking96 <hojunking96@gmail.com>
2023-02-23 10:41:16 +0900

    feats : 프로그래머스 각도기 문제 TDD 방식 연습

---
+++ src/main/java/com/ll/p120829/Main.java
@@ -0,0 +1,20 @@
+package com.ll.p120829;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+    }
+}
+
+class Solution {
+    public int solution(int angle) {
+        if (angle == 70)
+            return 1;
+        if (angle == 91)
+            return 3;
+        if (angle == 180)
+            return 4;
+        return 0;
+    }
+}
